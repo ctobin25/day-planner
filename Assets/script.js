4f92f62dec86bdffb19a5813366a90b350ef5889
caroline tobin <carolinetobin@carolines-MacBook-Pro.local>
2020-12-07 23:35:05 -0500

    addded current date to display and created table layout as well as save button function

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="jquery-3.5.1.min.js"></script>
-    <title>Day Planner</title>
-</head>
 
 
+$("#currentDay").text(moment().format('MMMM Do YYYY'));
+
+// class .
+// id # 
+// tag - element name
+$('.saveBtn').on( "click", saveData)
+
+function saveData(){
 
 
-<body>
+var saveKey = $(this).siblings('textarea').attr('id');
+var saveVal = $(this).siblings('textarea').val();
 
-$("#currentDay").text(luxon.DateTime.local());
-
-let currentTime = luxon.DateTime.local({
-  hour: "2-digit",
-});
+localStorage.setItem(saveKey, saveVal);
+}
 
 
-$("#currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
-
-
-
-
-
-
-<script src="luxon.js"></script>
-<script>luxon.DateTime.local();</script>
-<script src="https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js"></script>
-</body>
 
 
 
